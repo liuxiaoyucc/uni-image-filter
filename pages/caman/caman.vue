904aed6947e01cd4c0e7e9a194291dfb90f25bb3
xyliu <2199842341@qq.com>
2019-11-21 00:25:50 +0800

    裁剪原理

--- pages/caman/caman.vue
+++ pages/caman/caman.vue
@@ -6,31 +6,7 @@
 
 			<view class='time' v-if="gap">Processing time:{{gap}}ms</view>
 		</view>
-		<canvas style="width: 300px; height: 400px; background-color: #2C405A;" canvas-id="firstCanvas"></canvas>
-
-
-
-
-		<view class='wrap'>
-			<slider value="50" @change="sliderChange" activeColor="#FFCC33" backgroundColor="#000000" block-color="#8A6DE9"
-			 block-size="20" />
-			<button @tap="choose">choose image</button>
-			<!-- <button wx:if="{{selected}}" bindtap='filterTap'>filter</button> -->
-			<view v-if="selected" class='picker-wrap'>
-				<picker @change="bindPickerChange" :value="index" :range="picker_items">
-					<view class="picker">
-						当前选择：{{picker_items[index]}}
-					</view>
-				</picker>
-			</view>
-			<view class='time' v-if="gap">Processing time:{{gap}}ms</view>
-		</view>
-		<!-- <canvas canvas-id='hehe' style='margin:0 auto;width:320px; height:640px; border:1px solid #ddd;'></canvas> -->
-
-		<canvas canvas-id='hehe' :style="{width: upx2px(image.width)+ 'px', height: upx2px(image.height) +'px'}"></canvas>
-
-		<button v-if="selected" @tap="save" class='save'>save</button>
-
+		<canvas style="width: 750upx; height: 750upx; background-color: #2C405A;" canvas-id="firstCanvas"></canvas>
 
 
 
@@ -62,17 +38,21 @@
 					},
 				})
 			},
-			init_canvas(image_path) {
-				var context = uni.createCanvasContext('firstCanvas');
-
-
-				context.drawImage(image_path, 0, 0, 300, 400); //上
-				context.draw(false, function() {
-					new WxCaman('firstCanvas', 300, 400, function() {
-						this.vintage(false)
-						this.render()
-					})
-				})
+			init_canvas(path) {
+				var canvas = uni.createCanvasContext('firstCanvas');
+				/**
+				 * 0 || 360: 
+				 * 90度: (长, 0) 顶部朝向右侧
+				 * 180度 || -180度: (长, 宽)
+				 * -90度 || 270度: (0, 宽) 顶部朝向左侧
+				 */
+				canvas.translate(0, 0);
+				canvas.rotate(360 * Math.PI / 180);
+				canvas.drawImage(path, 0, 0, uni.upx2px(750), uni.upx2px(750));
+				
+				canvas.draw()
+				
+				
 			},
 		}
 	}
